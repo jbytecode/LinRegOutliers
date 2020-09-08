aa81658dc2e42668e31edb8eff40e1fbb9fc7941
Mehmet Hakan Satman <mhsatman@gmail.com>
2020-09-08 21:30:11 +0300

    docs for main algorithm of hs93

--- src/hs93.jl
+++ src/hs93.jl
@@ -34,7 +34,32 @@ function hs93basicsubset(setting::RegressionSetting, initialindices::Array{Int,1
     return indices
 end
 
+"""
 
+    hs93(setting; alpha = 0.05, basicsubsetindices = nothing)
+
+Perform the Hadi & Simonoff (1993) algorithm for the given regression setting.
+
+# Arguments
+- `setting::RegressionSetting`: RegressionSetting object with a formula and dataset.
+- `alpha::Float64`: Optional argument of the probability of rejecting the null hypothesis.
+- `basicsubsetindices::Array{Int, 1}`: Initial basic subset, by default, the algorithm creates an initial set of clean observations.
+
+# Examples
+```julia-repl
+julia> reg0001 = createRegressionSetting(@formula(calls ~ year), phones);
+julia> hs93(reg0001)
+ict{Any,Any} with 3 entries:
+  "outliers" => [14, 15, 16, 17, 18, 19, 20, 21]
+  "t"        => -3.59263
+  "d"        => [2.04474, 1.14495, -0.0633255, 0.0632934, -0.354349, -0.766818, -1.06862, -1.47638, -0.7…
+```
+
+# References
+Hadi, Ali S., and Jeffrey S. Simonoff. "Procedures for the identification of 
+multiple outliers in linear models." Journal of the American Statistical 
+Association 88.424 (1993): 1264-1272.
+"""
 function hs93(setting::RegressionSetting; alpha=0.05, basicsubsetindices=nothing)
     if basicsubsetindices === nothing
         initialsetindices = hs93initialset(setting)
