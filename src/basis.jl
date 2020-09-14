712aef2fadbf66d1553ae04ec6be0da23b0e2bb7
Mehmet Hakan Satman <mhsatman@gmail.com>
2020-09-14 22:02:17 +0300

    documentation for design matrix

--- src/basis.jl
+++ src/basis.jl
@@ -51,15 +51,108 @@ struct RegressionSetting
     data::DataFrame
 end
 
+
+"""
+
+    createRegressionSetting(formula, data)
+
+    Create a regression setting for a given formula and data
+
+# Arguments
+- `formula::FormulaTerm`: A formula object describes the linear regression model.
+- `data::DataFrame`: DataFrame object holds the data
+
+# Notes
+    Implemented methods in this packages accepts linear models as RegressionSetting objects.
+    This objects holds the model formula and the data used in regression estimations.
+    createRegressionSetting is a helper function for creating RegressionSetting objects.
+
+# Examples
+```julia-repl
+julia> setting = createRegressionSetting(@formula(calls ~ year), phones)
+RegressionSetting(calls ~ year, 24×2 DataFrame
+│ Row │ year  │ calls   │
+│     │ Int64 │ Float64 │
+├─────┼───────┼─────────┤
+│ 1   │ 50    │ 4.4     │
+│ 2   │ 51    │ 4.7     │
+│ 3   │ 52    │ 4.7     │
+│ 4   │ 53    │ 5.9     │
+│ 5   │ 54    │ 6.6     │
+│ 6   │ 55    │ 7.3     │
+│ 7   │ 56    │ 8.1     │
+│ 8   │ 57    │ 8.8     │
+│ 9   │ 58    │ 10.6    │
+│ 10  │ 59    │ 12.0    │
+⋮
+│ 14  │ 63    │ 21.2    │
+│ 15  │ 64    │ 119.0   │
+│ 16  │ 65    │ 124.0   │
+│ 17  │ 66    │ 142.0   │
+│ 18  │ 67    │ 159.0   │
+│ 19  │ 68    │ 182.0   │
+│ 20  │ 69    │ 212.0   │
+│ 21  │ 70    │ 43.0    │
+│ 22  │ 71    │ 24.0    │
+│ 23  │ 72    │ 27.0    │
+│ 24  │ 73    │ 29.0    │)
+```
+"""
 function createRegressionSetting(formula::FormulaTerm, data::DataFrame)::RegressionSetting
     return RegressionSetting(formula, data)
 end
 
+
+
+"""
+
+    designMatrix(setting)
+
+    Return matrix of independent variables including the variable (ones) of the constanst term for a given regression setting.
+
+# Arguments
+- `setting::RegressionSetting`: A regression setting object.
+
+# Notes
+    Design matrix is a matrix which holds values of independent variables on its columns for a given linear regression model.
+
+# Examples
+```julia-repl
+julia> setting = createRegressionSetting(@formula(calls ~ year), phones);
+julia> designMatrix(setting)
+24×2 Array{Float64,2}:
+ 1.0  50.0
+ 1.0  51.0
+ 1.0  52.0
+ 1.0  53.0
+ 1.0  54.0
+ 1.0  55.0
+ 1.0  56.0
+ 1.0  57.0
+ 1.0  58.0
+ 1.0  59.0
+ 1.0  60.0
+ 1.0  61.0
+ 1.0  62.0
+ 1.0  63.0
+ 1.0  64.0
+ 1.0  65.0
+ 1.0  66.0
+ 1.0  67.0
+ 1.0  68.0
+ 1.0  69.0
+ 1.0  70.0
+ 1.0  71.0
+ 1.0  72.0
+ 1.0  73.0
+```
+"""
 function designMatrix(setting::RegressionSetting)::Array{Float64,2}
     mf = ModelFrame(setting.formula, setting.data)
     mm = ModelMatrix(mf)
     return mm.m
 end
+
 
 function responseVector(setting::RegressionSetting)::Array{Float64,1}
     mf = ModelFrame(setting.formula, setting.data)
