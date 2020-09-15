117164d04ab1f209e81d1b18a021024241cdac32
Mehmet Hakan Satman <mhsatman@gmail.com>
2020-09-15 11:28:30 +0300

    documentation for bch

--- src/bch.jl
+++ src/bch.jl
@@ -1,9 +1,62 @@
+"""
+
+    coordinatwisemedians(datamat)
+
+    Return vector of medians of each column in a matrix.
+
+# Arguments
+- `datamat::Array{Float64, 2}`: A matrix.
+
+# Example
+```julia-repl
+julia> mat = [1.0 2.0; 3.0 4.0; 5.0 6.0]
+3×2 Array{Float64,2}:
+ 1.0  2.0
+ 3.0  4.0
+ 5.0  6.0
+
+julia> coordinatwisemedians(mat)
+2-element Array{Float64,1}:
+ 3.0
+ 4.0
+```
+"""
 function coordinatwisemedians(datamat::Array{Float64,2})::Array{Float64,1}
     n, p = size(datamat)
     meds = map(i -> median(datamat[:, i]), 1:p)
     return meds
 end
 
+
+"""
+
+    bch(setting; alpha = 0.05, maxiter = 1000, epsilon = 0.000001)
+
+Perform the Billor & Chatterjee & Hadi (2006) algorithm for the given regression setting.
+
+# Arguments
+- `setting::RegressionSetting`: RegressionSetting object with a formula and dataset.
+- `alpha::Float64`: Optional argument of the probability of rejecting the null hypothesis.
+- `maxiter::Int`: Maximum number of iterations for calculating iterative weighted least squares estimates.
+- `epsilon::Float64`: Accuracy for determining convergency.
+
+# Examples
+```julia-repl
+julia> reg  = createRegressionSetting(@formula(calls ~ year), phones);
+julia> Dict{Any,Any} with 7 entries:
+"betas"                               => [-55.9205, 1.15572]
+"squared.normalized.robust.distances" => [0.104671, 0.0865052, 0.0700692, 0.0553633, 0.0423875, 0.03…
+"weights"                             => [0.00186158, 0.00952088, 0.0787321, 0.0787321, 0.0787321, 0…
+"outliers"                            => [1, 14, 15, 16, 17, 18, 19, 20, 21]
+"squared.normalized.residuals"        => [5.53742e-5, 2.42977e-5, 2.36066e-6, 2.77706e-6, 1.07985e-7…
+"residuals"                           => [2.5348, 1.67908, 0.523367, 0.567651, 0.111936, -0.343779, …
+"basic.subset"                        => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10  …  15, 16, 17, 18, 19, 20, …
+```
+
+# References
+Billor, Nedret, Samprit Chatterjee, and Ali S. Hadi. "A re-weighted least squares method 
+for robust regression estimation." American journal of mathematical and management sciences 26.3-4 (2006): 229-252.
+"""
 function bch(setting::RegressionSetting; alpha=0.05, maxiter=1000, epsilon=0.000001)
     Xdesign = designMatrix(setting)
     Y = responseVector(setting)
@@ -107,6 +160,26 @@ function bch(setting::RegressionSetting; alpha=0.05, maxiter=1000, epsilon=0.000
 end
 
 
+
+
+"""
+
+    bchplot(setting::RegressionSetting; alpha=0.05, maxiter=1000, epsilon=0.00001)
+
+Perform the Billor & Chatterjee & Hadi (2006) algorithm and generates outlier plot 
+for the given regression setting.
+
+# Arguments
+- `setting::RegressionSetting`: RegressionSetting object with a formula and dataset.
+- `alpha::Float64`: Optional argument of the probability of rejecting the null hypothesis.
+- `maxiter::Int`: Maximum number of iterations for calculating iterative weighted least squares estimates.
+- `epsilon::Float64`: Accuracy for determining convergency.
+
+
+# References
+Billor, Nedret, Samprit Chatterjee, and Ali S. Hadi. "A re-weighted least squares method 
+for robust regression estimation." American journal of mathematical and management sciences 26.3-4 (2006): 229-252.
+"""
 function bchplot(setting::RegressionSetting; alpha=0.05, maxiter=1000, epsilon=0.00001)
     result = bch(setting, alpha=alpha, maxiter=maxiter, epsilon=epsilon)
     squared_normalized_residuals = result["squared.normalized.residuals"]
