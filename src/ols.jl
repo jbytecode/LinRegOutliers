0087efb6f51763744c0ffe45f6930dda8773e2fc
jbytecode <mhsatman@gmail.com>
2020-10-07 11:52:09 +0300

    OLS type for fast linear regression in sub-algorithms

---
+++ src/ols.jl
@@ -0,0 +1,25 @@
+struct OLS
+    X::Array{Float64,2}
+    y::Array{Float64,1}
+    betas::Array{Float64,1}
+end
+
+OLS(X::Array{Float64,2}, y::Array{Float64,1}) = OLS(X, y, inv(X' * X) * X' * y)
+
+function WLS(X::Array{Float64,2}, y::Array{Float64,1}, wts::Array{Float64,1}) 
+    n = length(y)
+    W = zeros(Float64, n, n)
+    for i in 1:n
+        W[i, i] = wts[i]
+    end
+    return OLS(X, y, inv(X' * W * X) * X' * W * y)
+end 
+
+residuals(ols::OLS) = ols.y .- ols.X * ols.betas
+
+coef(ols::OLS) = ols.betas
+
+predict(ols::OLS) = ols.X * ols.betas
+
+predict(ols::OLS, X::Array{Float64,2}) = X * ols.betas
+
