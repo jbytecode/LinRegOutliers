98db0fc9af663c7d34bdc67788add7dcd06e4db8
jbytecode <mhsatman@gmail.com>
2020-10-12 22:37:11 +0300

    docs for covratio

--- src/basis.jl
+++ src/basis.jl
@@ -254,7 +254,22 @@ function find_minimum_nonzero(arr::Array{Float64,1})
 end
 
 
+"""
+    covratio(setting, omittedIndex)
 
+Apply covariance ratio diagnostic for a given regression setting and observation index.
+
+# Arguments
+- `setting::RegressionSetting`: A regression setting object.
+- `omittedIndex::Int`: Index of the omitted observation.
+
+# Example
+```julia-repl
+julia> setting = createRegressionSetting(@formula(calls ~ year), phones);
+julia> covratio(setting, 1)
+1.2945913799871505
+```
+"""
 function covratio(setting::RegressionSetting, omittedIndex::Int)
     X = designMatrix(setting)
     y = responseVector(setting)
