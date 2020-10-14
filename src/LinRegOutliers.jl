408f348e7cd5ee0645d2fc52e9dfb21138ed2388
jbytecode <mhsatman@gmail.com>
2020-10-14 18:54:10 +0300

    wls() revisited with Diagonal in Linear Algebra, new test for wls

--- src/LinRegOutliers.jl
+++ src/LinRegOutliers.jl
@@ -15,7 +15,7 @@ import DataFrames: DataFrame
 import Distributions: TDist, Chisq, Normal, std, cov, median
 import Clustering: Hclust, hclust, cutree
 import StatsBase: quantile, standardize, ZScoreTransform, mean, sample
-import LinearAlgebra: inv, pinv, det, diag, eigen, norm
+import LinearAlgebra: inv, pinv, det, diag, eigen, norm, Diagonal
 import Plots: scatter, title!, xlabel!, ylabel!, hline!, vline!, RGB, plot, font, text
 import Optim: optimize, NelderMead
 import Combinatorics: combinations
