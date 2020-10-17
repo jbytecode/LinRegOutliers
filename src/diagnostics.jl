ee0c5aed8bfe3306a5435601c58f89ffab178c6e
jbytecode <mhsatman@gmail.com>
2020-10-17 22:57:49 +0300

    doc info for Hadi's measure

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -467,7 +467,25 @@ function covratio(X::Array{Float64,2}, y::Array{Float64,1}, omittedIndex::Int)
 end
 
 
+"""
+    hadimeasure(setting; c = 2.0)
 
+Apply Hadi's regression diagnostic for a given regression setting
+
+# Arguments
+- `setting::RegressionSetting`: A regression setting object.
+- `c::Float64`: Critical value selected between 2.0 - 3.0. The default is 2.0.
+
+# Example
+```julia-repl
+julia> setting = createRegressionSetting(@formula(calls ~ year), phones);
+julia> hadimeasure(setting)
+```
+
+# References
+Chatterjee, Samprit and Hadi, Ali. Regression Analysis by Example.
+     5th ed. N.p.: John Wiley & Sons, 2012.
+"""
 function hadimeasure(setting::RegressionSetting; c::Float64=2.0)
     X = designMatrix(setting)
     y = responseVector(setting)
