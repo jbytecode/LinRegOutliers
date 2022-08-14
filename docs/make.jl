317db4324ac04e31bb5738a4ac7414ee12a19cf9
jbytecode <mhsatman@gmail.com>
2022-08-14 16:04:54 +0300

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,26 @@
-using Documenter
+using Documenter, LinRegOutliers
 
-using Pkg
-Pkg.activate("../")
-using LinRegOutliers
 
-push!(LOAD_PATH,"../src/")
 makedocs(
-	sitename="LinRegOutliers",
-	pages=[
-		"datasets.md",
-		"types.md",
-		"diagnostics.md",
-		"algorithms.md"
-	]
+         format = Documenter.HTML(
+                                  prettyurls = get(ENV, "CI", nothing) == "true",
+                                  collapselevel = 2,
+                                  # assets = ["assets/favicon.ico", "assets/extra_styles.css"],
+                                 ),
+         sitename="LinRegOutliers",
+         authors = "Mehmet Hakan Satman <mhsatman@gmail.com>, Shreesh Adiga <16567adigashreesh@gmail.com>, Guillermo Angeris <angeris@stanford.edu>, Emre Akadal <emre.akadal@istanbul.edu.tr>",
+         pages = [
+                  "Algorithms" => "algorithms.md",
+                  "Diagnostics" =>  "diagnostics.md",
+                  "Types" => "types.md",
+                  "Datasets" => "datasets.md"
+                 ]
+        )
+
+
+deploydocs(
+   repo = "github.com/jbytecode/LinRegOutliers",
 )
+
 
 
