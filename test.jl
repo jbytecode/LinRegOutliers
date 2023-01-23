369272494c3b583971ffe2e3c89c4fb1d7417990
hako <mhsatman@gmail.com>
2023-01-23 11:38:46 +0300

    improve lts performance

--- test.jl
+++ test.jl
@@ -3,16 +3,13 @@ using LinRegOutliers
 
 
 #sett = createRegressionSetting(@formula(calls ~ year), phones)
-sett = createRegressionSetting(@formula(y ~ x1 + x2 + x3), hbk)
+#sett = createRegressionSetting(@formula(y ~ x1 + x2 + x3), hbk)
+
+n = 1000
+betas = [5.0 for i in 1:10]
+x = rand(n, 10)
+e = randn(n)
+y = x * betas + e
 
 
-#n = 30
-#x1 = rand(n)
-#x2 = rand(n)
-#e = randn(n)
-#y = 5 .+ 5 .* x1 + 5 .* x2 + e 
-#y[end] = y[end] * 10
-#mydata = DataFrame("x1" => x1, "x2" => x2, "y" => y)
-#sett = createRegressionSetting(@formula(y ~ x1 + x2), mydata)
-
-result = diagnose(sett)
+result = lts(x, y)
